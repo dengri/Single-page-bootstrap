c9d7b55a0aeaea5efdd00de137c733d35a716ee5
Dengri <grischenkodenis@gmail.com>
2015-07-25 02:43:37 +0300

    Adding Smooth scroll

--- js/myscript.js
+++ js/myscript.js
@@ -30,6 +30,23 @@ $(function(){
 	
 	});
 
+  //Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') === 
+            this.pathname.replace(/^\//,'') && 
+                  location.hostname === this.hostname) {
+                        var target = $(this.hash);
+                        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                        if (target.length) {
+                            $('html,body').animate({
+                                 scrollTop: target.offset().top+2
+                             }, 500);
+                             return false;
+                             } //target.length
+                             } //click function
+                             }); //smooth scrolling
+
+
 
 
 	
